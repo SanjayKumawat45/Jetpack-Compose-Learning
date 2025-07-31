242f7c12a8626a323bc3c966cd0e1f29a80df977
SanjayKumawat45 <sanjay4kumawat@gmail.com>
2025-07-31 22:50:32 +0530

    Counter Screen for learning State

--- app/src/main/java/com/example/jetpackcomposelearning/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposelearning/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,14 +22,21 @@ import com.example.jetpackcomposelearning.ui.theme.JetpackComposeLearningTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             JetpackComposeLearningTheme {
-                Column {
-//                    Greet("guys")
-                    ProfileCard("Sanjay",20, Color.Companion.Cyan)
+               Column(modifier = Modifier.fillMaxSize(),
+                   verticalArrangement = Arrangement.Center,
+                   horizontalAlignment = Alignment.CenterHorizontally
+                   ) {
+                   CounterScreen()
+////                    Greet("guys")
+//                    ProfileCard("Sanjay",20, Color.Companion.Cyan)
+//
+                  }
 
-                }
+
+
             }
         }
     }
